783801d6f6a29e4ab54d200e3f9accf7b9006330
Brandon K. Waltz <bwaltz6@gmail.com>
2025-10-23 14:01:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,37 +3,39 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-export default defineConfig({
-    plugins: [vue()],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        watch: { usePolling: true, interval: 100 },
-        origin: 'https://colby.lndo.site',
-        css: {
-            postcss: './postcss.config.js',
-        },
-        // HMR through same origin proxy still uses the internal host
-        hmr: {
-            protocol: 'wss',
-            host: 'colby.lndo.site',
-            clientPort: 443,
-            path: '/vite',
-        },
-        allowedHosts: ['.lndo.site', 'localhost', 'node'],
-    },
-    build: {
-        emptyOutDir: false,
-        manifest: true,
-        rollupOptions: {
-            input: path.resolve(__dirname, 'resources/js/app.js'),
-            output: {
-                dir: path.resolve(__dirname, 'dist'),
-                format: 'es',
+export default defineConfig(({ command, mode }) => {
+    return {
+        plugins: [vue()],
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            strictPort: true,
+            watch: { usePolling: true, interval: 100 },
+            origin: 'https://colby.lndo.site',
+            css: {
+                postcss: './postcss.config.js',
             },
+            // HMR through same origin proxy still uses the internal host
+            hmr: {
+                protocol: 'wss',
+                host: 'colby.lndo.site',
+                clientPort: 443,
+                path: '/vite',
+            },
+            allowedHosts: ['.lndo.site', 'localhost', 'node'],
         },
-        outDir: 'dist',
-    },
-    base: command === 'build' ? '/wp-content/themes/colby-college-theme-inertia/dist/' : '/vite/',
+        build: {
+            emptyOutDir: false,
+            manifest: true,
+            rollupOptions: {
+                input: path.resolve(__dirname, 'resources/js/app.js'),
+                output: {
+                    dir: path.resolve(__dirname, 'dist'),
+                    format: 'es',
+                },
+            },
+            outDir: 'dist',
+        },
+        base: command === 'build' ? '/wp-content/themes/colby-college-theme-inertia/dist/' : '/vite/',
+    }
 });
