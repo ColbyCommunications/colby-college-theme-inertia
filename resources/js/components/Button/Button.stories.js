518fcd812f4ad90fd6f666ea239620faff0de469
Brandon K. Waltz <bwaltz6@gmail.com>
2026-01-26 12:02:51 -0500

    Create Button.stories.js

---
+++ resources/js/components/Button/Button.stories.js
@@ -0,0 +1,79 @@
+import Button from "./Button.vue";
+
+// The default export metadata for your component
+export default {
+  title: "Button",
+  component: Button,
+};
+
+export const Primary = {
+  name: "Primary",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+  },
+};
+
+export const small = {
+  name: "Small",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    size: "small",
+  },
+};
+
+export const medium = {
+  name: "Medium",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    size: "medium",
+  },
+};
+
+export const large = {
+  name: "Large",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    size: "large",
+  },
+};
+
+export const dark = {
+  name: "Dark",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    type: "dark",
+  },
+};
+
+export const withArrow = {
+  name: "With Arrow",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    arrow: true,
+  },
+};
+
+export const reverse = {
+  name: "Reverse",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    arrow: true,
+    reverse: true,
+  },
+};
+
+export const transparent = {
+  name: "Transparent Background",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    transparentBg: true,
+  },
+};
