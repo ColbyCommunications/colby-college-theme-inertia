7d76cd179fffb5e97339967830ba4a0b67e5dbc6
bwaltz <bwaltz6@gmail.com>
2026-01-27 21:33:17 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@3111ac376263cc76d02f30d2aa1df1071269e7bd 🚀

---
+++ assets/Breadcrumbs.stories-DQfUx7oH.js
@@ -0,0 +1,18 @@
+import{a as s,b as e,d as c,F as u,j as m,o,t as d}from"./iframe-BET2YoQW.js";import{_ as p}from"./Icon-DZxh5Vkt.js";import"./preload-helper-PPVm8Dsz.js";const w={"aria-label":"Breadcrumbs",class:"breadcrumbs flex h-9 items-center overflow-x-auto bg-gray-100"},f={class:"breadcrumbs__inner mx-auto w-full max-w-screen-2xl px-5"},b={class:"flex items-center [&>li+li]:ml-5"},_={class:"font-body text-10 leading-140 font-bold whitespace-nowrap text-indigo-800"},y={class:"text-indigo-800 transition-all duration-200 ease-in-out hover:text-indigo",href:"/"},h={class:"size-[12px] fill-indigo"},g=["href"],n={__name:"Breadcrumbs",props:{items:{type:Array,default:()=>[]}},setup(i){return(x,r)=>(o(),s("nav",w,[e("div",f,[e("ul",b,[e("li",_,[e("a",y,[r[0]||(r[0]=e("span",{class:"sr-only"},"Homepage",-1)),e("div",h,[c(p,{name:"home"})])])]),(o(!0),s(u,null,m(i.items,(a,l)=>(o(),s("li",{key:l,class:"font-body text-10 leading-140 font-bold whitespace-nowrap text-indigo-800"},[e("a",{class:"text-indigo-800 transition-all duration-200 ease-in-out hover:text-indigo",href:a.url},d(a.title),9,g)]))),128))])])]))}};n.__docgenInfo={exportName:"default",displayName:"Breadcrumbs",description:"",tags:{},props:[{name:"items",type:{name:"array"},defaultValue:{func:!1,value:"[]"}}],sourceFiles:["/home/runner/work/colby-college-theme-inertia/colby-college-theme-inertia/resources/js/components/Breadcrumbs/Breadcrumbs.vue"]};const k={title:"Core Components/Breadcrumbs",component:n},t={name:"Default",args:{items:[{title:"Admissions",url:"https://www.colby.edu"},{title:"Museum",url:"https://www.colby.edu"},{title:"myColby",url:"https://www.colby.edu"},{title:"Office of Communications",url:"https://www.colby.edu"}]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  name: "Default",
+  args: {
+    items: [{
+      title: "Admissions",
+      url: "https://www.colby.edu"
+    }, {
+      title: "Museum",
+      url: "https://www.colby.edu"
+    }, {
+      title: "myColby",
+      url: "https://www.colby.edu"
+    }, {
+      title: "Office of Communications",
+      url: "https://www.colby.edu"
+    }]
+  }
+}`,...t.parameters?.docs?.source}}};const N=["Primary"];export{t as Primary,N as __namedExportsOrder,k as default};
