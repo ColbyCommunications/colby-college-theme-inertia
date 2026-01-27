082d1ac646dd9ca73029e35c8efcf163992586df
bwaltz <bwaltz6@gmail.com>
2026-01-27 19:19:33 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@a1129c4fe7fc49a45603de06cb9e66db09b491db 🚀

--- assets/ArrowControls.stories-CMpSjTJU.js
+++
@@ -1,4 +0,0 @@
-import{_ as o}from"./ArrowControls-uovr9P3b.js";import"./iframe-DnZ0NymE.js";import"./preload-helper-PPVm8Dsz.js";import"./ArrowButton-oWE27L4t.js";const m={title:"Core Components/Arrow Controls",component:o},r={name:"Primary"};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  // 👇 Rename this story
-  name: "Primary"
-}`,...r.parameters?.docs?.source}}};const n=["Primary"];export{r as Primary,n as __namedExportsOrder,m as default};
---
+++ assets/Article.stories-DNVHV7eA.js
@@ -0,0 +1,21 @@
+import{_ as e}from"./Article-BuxuXH6x.js";import"./iframe-BRCbDJXs.js";import"./preload-helper-PPVm8Dsz.js";import"./TextGroup-DZPBnKNS.js";import"./ButtonGroup-BdhJaYNW.js";import"./Button-h8JpxMaA.js";const n={title:"Core Components/Article",component:e},t={name:"Primary",args:{heading:"Lorem Ipsum",subheading:"adipisci amet",paragraph:"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",image:{src:"https://placeholdit.com/600x400/dddddd/999999",srcset:"https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",alt:"this is an alt tag"},buttons:[{button:{size:"small",title:"button1",url:"https://www.colby.edu"}}]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Primary",
+  args: {
+    heading: "Lorem Ipsum",
+    subheading: "adipisci amet",
+    paragraph: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    image: {
+      src: "https://placeholdit.com/600x400/dddddd/999999",
+      srcset: "https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",
+      alt: "this is an alt tag"
+    },
+    buttons: [{
+      button: {
+        size: "small",
+        title: "button1",
+        url: "https://www.colby.edu"
+      }
+    }]
+  }
+}`,...t.parameters?.docs?.source}}};const m=["Primary"];export{t as Primary,m as __namedExportsOrder,n as default};
