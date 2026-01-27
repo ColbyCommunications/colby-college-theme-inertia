b4ea65a1d2630184d5b45f6fdf9e7321ba848872
bwaltz <bwaltz6@gmail.com>
2026-01-27 22:04:19 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@5b2f42d46857c3d867cfd32fc1b3283bd6aff460 🚀

---
+++ assets/Button.stories-BNNdevig.js
@@ -0,0 +1,57 @@
+import{_ as l}from"./Button-_sqvmeXP.js";import"./iframe-E2fnaGNL.js";import"./preload-helper-PPVm8Dsz.js";const p={title:"Core Components/Button",component:l},e={name:"Primary",args:{title:"Button Title",url:"https://www.colby.edu"}},r={name:"Small",args:{title:"Button Title",url:"https://www.colby.edu",size:"small"}},t={name:"Medium",args:{title:"Button Title",url:"https://www.colby.edu",size:"medium"}},a={name:"Large",args:{title:"Button Title",url:"https://www.colby.edu",size:"large"}},s={name:"Dark",args:{title:"Button Title",url:"https://www.colby.edu",type:"dark"}},n={name:"With Arrow",args:{title:"Button Title",url:"https://www.colby.edu",arrow:!0}},o={name:"Reverse",args:{title:"Button Title",url:"https://www.colby.edu",arrow:!0,reverse:!0}},u={name:"Transparent Background",args:{title:"Button Title",url:"https://www.colby.edu",transparentBg:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: "Primary",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu"
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  name: "Small",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    size: "small"
+  }
+}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  name: "Medium",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    size: "medium"
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  name: "Large",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    size: "large"
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  name: "Dark",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    type: "dark"
+  }
+}`,...s.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  name: "With Arrow",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    arrow: true
+  }
+}`,...n.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  name: "Reverse",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    arrow: true,
+    reverse: true
+  }
+}`,...o.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: "Transparent Background",
+  args: {
+    title: "Button Title",
+    url: "https://www.colby.edu",
+    transparentBg: true
+  }
+}`,...u.parameters?.docs?.source}}};const w=["Primary","small","medium","large","dark","withArrow","reverse","transparent"];export{e as Primary,w as __namedExportsOrder,s as dark,p as default,a as large,t as medium,o as reverse,r as small,u as transparent,n as withArrow};
