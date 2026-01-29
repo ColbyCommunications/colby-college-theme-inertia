2ea7f129b1db7851eef0f7e817236e5c19a1577e
bwaltz <bwaltz6@gmail.com>
2026-01-29 19:44:25 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@76c149d1194885e788d62376af72923e233832d6 🚀

---
+++ assets/ArrowControls.stories-DcOBY4Qb.js
@@ -0,0 +1,4 @@
+import{_ as o}from"./ArrowControls-Bi4eBEf4.js";import"./iframe-BzNJ8ej3.js";import"./preload-helper-PPVm8Dsz.js";import"./ArrowButton-DT8RA3ev.js";const m={title:"Core Components/Arrow Controls",component:o},r={name:"Primary"};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Primary"
+}`,...r.parameters?.docs?.source}}};const n=["Primary"];export{r as Primary,n as __namedExportsOrder,m as default};
