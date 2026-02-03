00a7208343eda81e64d921bb8cc33910f8c4b6a4
Brandon K. Waltz <bwaltz6@gmail.com>
2026-02-03 16:07:54 -0500

    updates

---
+++ resources/js/components/Embed/Embed.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="embed">
+    <div class="embed__main">
+      <div v-html="embed"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  // Data Objects
+  embed: {
+    type: String,
+    default: "",
+  },
+});
+</script>
