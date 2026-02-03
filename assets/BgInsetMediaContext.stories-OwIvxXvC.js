53a83ec30ccd2ac5a79ec366b8a9ccf1e3e3e11d
bwaltz <bwaltz6@gmail.com>
2026-02-03 19:26:56 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@b7791269206d2aef46e7e856d87bbdeb40791223 🚀

---
+++ assets/BgInsetMediaContext.stories-OwIvxXvC.js
@@ -0,0 +1,37 @@
+import{c as d,a as n,e as m,q as l,s as p,d as u}from"./iframe-7uD2a8ZT.js";import{_ as g}from"./MediaContext-D9_leplm.js";import"./preload-helper-PPVm8Dsz.js";import"./Video-CmTnqB0J.js";import"./Icon-C3jKa57R.js";import"./Picture-DTpOp4N-.js";import"./Context-CQIFRsi9.js";import"./TextGroup-PBIYzyPI.js";import"./ButtonGroup-gYAAuFwZ.js";import"./Button-DjixpZEP.js";const _={class:"bg-inset-media-context"},x={class:"bg-inset-media-context__inner mx-auto my-0 w-full max-w-screen-2xl gap-x-10 px-5 md:grid md:grid-cols-12"},h={class:"bg-inset-media-context__main md:col-span-12"},b={class:"bg-inset-media-context__bg bg-hero__pattern bg-cover bg-no-repeat px-8 py-14 md:px-14"},i={__name:"BgInsetMediaContext",setup(v){return(c,D)=>(u(),d("div",_,[n("div",x,[n("div",h,[n("div",b,[m(g,l(p(c.$attrs)),null,16)])])])]))}};i.__docgenInfo={exportName:"default",displayName:"BgInsetMediaContext",description:"",tags:{},sourceFiles:["/home/runner/work/colby-college-theme-inertia/colby-college-theme-inertia/resources/js/components/BgInsetMediaContext/BgInsetMediaContext.vue"]};const L={title:"Core Components/BG Inset Media Context",component:i},e={heading:"Lorem Ipsum",subheading:"Veritatis sit voluptatem",image:{src:"https://placeholdit.com/600x400/dddddd/999999",srcset:"https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",alt:"this is an alt tag",sizes:{Hero:"https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg",mobile:"https://placehold.co/300x200"}},paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},t={name:"Default",args:{...e}},s={name:"Inset",args:{...e,inset:!0}},a={name:"Reverse",args:{...e,reverse:!0}},r={name:"Large",args:{...e,size:"large"}},o={name:"Video",args:{...e,video:{id:"jQixf1UCKac"}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Default",
+  args: {
+    ...globalArgs
+  }
+}`,...t.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Inset",
+  args: {
+    ...globalArgs,
+    inset: true
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Reverse",
+  args: {
+    ...globalArgs,
+    reverse: true
+  }
+}`,...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Large",
+  args: {
+    ...globalArgs,
+    size: "large"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Video",
+  args: {
+    ...globalArgs,
+    video: {
+      id: "jQixf1UCKac"
+    }
+  }
+}`,...o.parameters?.docs?.source}}};const M=["Primary","Inset","Reverse","Large","Video"];export{s as Inset,r as Large,t as Primary,a as Reverse,o as Video,M as __namedExportsOrder,L as default};
---
+++ assets/Video.stories-CxS4TroW.js
@@ -0,0 +1,16 @@
+import{_ as d}from"./Video-CmTnqB0J.js";import"./iframe-7uD2a8ZT.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-C3jKa57R.js";import"./Picture-DTpOp4N-.js";const i={title:"Core Components/Video",component:d},t={name:"Default",args:{id:"jQixf1UCKac",image:{src:"https://placeholdit.com/600x400/dddddd/999999",srcset:"https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",alt:"this is an alt tag",sizes:{Hero:"https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg",mobile:"https://placehold.co/300x200"}}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  // 👇 Rename this story
+  name: "Default",
+  args: {
+    id: "jQixf1UCKac",
+    image: {
+      src: "https://placeholdit.com/600x400/dddddd/999999",
+      srcset: "https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",
+      alt: "this is an alt tag",
+      sizes: {
+        Hero: "https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg",
+        mobile: "https://placehold.co/300x200"
+      }
+    }
+  }
+}`,...t.parameters?.docs?.source}}};const p=["Primary"];export{t as Primary,p as __namedExportsOrder,i as default};
