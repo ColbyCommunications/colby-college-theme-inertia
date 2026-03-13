daac7dbb98e55ebbe0da05ac1f9847b93f448a65
bwaltz <bwaltz6@gmail.com>
2026-03-13 18:06:54 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@711a3ed452b5d9812b33c34cde7b5f9b76c0fed9 🚀

---
+++ assets/Wysiwyg.stories-zUQpvsHI.js
@@ -0,0 +1,12 @@
+import{_ as a}from"./Wysiwyg-BMXo9Kee.js";import"./iframe-nsPjl7RS.js";import"./preload-helper-PPVm8Dsz.js";const{expect:i}=__STORYBOOK_MODULE_TEST__,r={title:"Core Components/Wysiwyg",component:a,tags:["autodocs"]},e={name:"Default",args:{content:'<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2><p>Lorem ipsum dolor sit amet, <a href="/">consectetur</a> adipiscing elit. Sed leo odio, ultrices quis auctor eu, fermentum at dolor. Vivamus cursus lorem ut ultrices iaculis. Phasellus et ante nec urna facilisis fringilla.</p><h3>Spring 2023</h3><p>Fusce tincidunt, magna sit amet varius porttitor, orci metus lacinia mi, vel vulputate nisl diam lacinia augue. In lacinia nisl a nisl condimentum laoreet.</p>'},play:async({canvas:t})=>{await i(t.getByText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")).toBeInTheDocument(),await i(t.getByText("Spring 2023")).toBeInTheDocument()}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: "Default",
+  args: {
+    content: '<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2><p>Lorem ipsum dolor sit amet, <a href="/">consectetur</a> adipiscing elit. Sed leo odio, ultrices quis auctor eu, fermentum at dolor. Vivamus cursus lorem ut ultrices iaculis. Phasellus et ante nec urna facilisis fringilla.</p><h3>Spring 2023</h3><p>Fusce tincidunt, magna sit amet varius porttitor, orci metus lacinia mi, vel vulputate nisl diam lacinia augue. In lacinia nisl a nisl condimentum laoreet.</p>'
+  },
+  play: async ({
+    canvas
+  }) => {
+    await expect(canvas.getByText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")).toBeInTheDocument();
+    await expect(canvas.getByText("Spring 2023")).toBeInTheDocument();
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,r as default};
