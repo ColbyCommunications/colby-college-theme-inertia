525c2e1b075a1ca92ba88cefc6e19186d2669393
Brandon K. Waltz <bwaltz6@gmail.com>
2026-04-06 13:09:37 -0400

    Update page.php

--- page.php
+++ page.php
@@ -9,15 +9,28 @@ $filtered_blocks = array_filter( parse_blocks($post->post_content), function( $b
     return ! is_null( $block['blockName'] );
 });
 
+function get_structured_block_data($block) {
+  // 1. Setup temporary metadata so ACF functions work
+  acf_setup_meta($block['attrs']['data'], $block['attrs']['id'] ?? 'block_temp', true);
+  
+  // 2. Use get_fields() to get the fully hydrated, nested tree
+  $fields = get_fields(); // This now returns repeaters as proper arrays of objects
+  
+  // 3. Clean up
+  acf_reset_meta($block['attrs']['id'] ?? 'block_temp');
+  
+  return $fields;
+}
+
+foreach ($filtered_blocks as &$block) {
+  $block['attrs']['data'] = get_structured_block_data($block);
+}
+
 // dd($filtered_blocks);
-
-$blocks = prepare_data($filtered_blocks);
-
-
 
 Inertia::render('Page/Show', [
   'id'     => $post->ID,
   'title'  => get_the_title($post->ID),
   'layout' => 'default',
-  'blocks' => $blocks,
+  'blocks' => $filtered_blocks,
 ]);
