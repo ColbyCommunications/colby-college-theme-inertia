16b234e3cccef12f403ee02e0cd5716b4c693dab
Brandon K. Waltz <bwaltz6@gmail.com>
2026-04-13 14:37:05 -0400

    Update ArticleSectionBlock.stories.js

--- resources/js/components/ArticleSection/storybook/ArticleSectionBlock.stories.js
+++ resources/js/components/ArticleSection/storybook/ArticleSectionBlock.stories.js
@@ -3,83 +3,123 @@ import { expect } from "storybook/test";
 
 const mockItems = [
   {
-    image: {
-      src: "https://placeholdit.com/600x400/dddddd/999999",
-      srcset:
-        "https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",
-      alt: "this is an alt tag",
+    yoast_head_json: {
+      og_image: [
+        {
+          url: "",
+          src: ""
+        }
+      ],
+      og_description: ""
     },
-    heading: "Future of Artificial Intelligence",
-    subheading: "Technology",
-    paragraph: "AI is reshaping industries at an unprecedented pace.",
-    url: "#",
-    // Structure required for Accordion style images
-    //
-    sizes: {
-      desktop: "https://placehold.co/600x400",
-      mobile: "https://placehold.co/300x200",
+    title: {
+      rendered: "Burke Has New Essay, Published in &lt;em>Panorama&lt;/em>"
     },
-    buttons: [
-      {
-        button: {
-          size: "small",
-          title: "button1",
-          url: "https://www.colby.edu",
-        },
-      },
-    ],
+    ['post-meta-fields']: {
+      primary_category: "",
+      summary: [
+        "Michael Burke, professor of English and creative writing, emeritus, has a new essay that appeared in <em>Panorama: The J..."
+      ]
+    },
+    guid: {
+      rendered: "https://panoramajournal.org/issues/issue-17-reflections/reflections-weapon-station/"
+    }
   },
   {
-    image: {
-      src: "https://placeholdit.com/600x400/dddddd/999999",
-      srcset:
-        "https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",
-      alt: "this is an alt tag",
+    yoast_head_json: {
+      og_image: [
+        {
+          url: "",
+          src: ""
+        }
+      ],
+      og_description: ""
     },
-    heading: "Sustainable Energy Solutions",
-    subheading: "Environment",
-    paragraph: "Solar and wind energy are becoming more accessible than ever.",
-    url: "#",
-    sizes: {
-      desktop: "https://placehold.co/600x400",
-      mobile: "https://placehold.co/300x200",
+    title: {
+      rendered: "Funahashi and Martínez Visit Music Conservatories in Italy"
     },
-    buttons: [
-      {
-        button: {
-          size: "small",
-          title: "button1",
-          url: "https://www.colby.edu",
-        },
-      },
-    ],
+    ['post-meta-fields']: {
+      primary_category: "",
+      summary: [
+        "In March, pianist and Associate Professor of Music Yuri &#8220;Lily&#8221; Funahashi and composer, percussionist, and As..."
+      ]
+    },
+    guid: {
+      rendered: "https://www.conservatoriovenezia.eu/blog/eventi/masterclass/lecture-j-martinez/"
+    }
   },
   {
-    image: {
-      src: "https://placeholdit.com/600x400/dddddd/999999",
-      srcset:
-        "https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",
-      alt: "this is an alt tag",
+    yoast_head_json: {
+      og_image: [
+        {
+          url: "",
+          src: ""
+        }
+      ],
+      og_description: ""
     },
-    heading: "Modern Architecture Trends",
-    subheading: "Design",
-    paragraph: "Exploring the intersection of function and aesthetics in 2024.",
-    url: "#",
-    sizes: {
-      desktop: "https://placehold.co/600x400",
-      mobile: "https://placehold.co/300x200",
+    title: {
+      rendered: "Falantin and Firkusny &#8217;25 Coauthor Article in &lt;em>The French Review&lt;/em>"
     },
-    buttons: [
-      {
-        button: {
-          size: "small",
-          title: "button1",
-          url: "https://www.colby.edu",
-        },
-      },
-    ],
+    ['post-meta-fields']: {
+      primary_category: "",
+      summary: [
+        "Assistant Professor of French Studies Flavien Falantin and Alex Firkusny &#8217;25 have coauthored a journal article rec..."
+      ]
+    },
+    guid: {
+      rendered: "https://muse.jhu.edu/pub/1/article/984103/pdf"
+    }
   },
+  {
+    yoast_head_json: {
+      og_image: [
+        {
+          url: "",
+          src: ""
+        }
+      ],
+      og_description: ""
+    },
+    title: {
+      rendered: "Braunstein&#8217;s Upcoming Book Received Starred Review from Kirkus"
+    },
+    ['post-meta-fields']: {
+      primary_category: "",
+      summary: [
+        "An upcoming book of short stories by Sarah Braunstein, associate professor of creative writing, received a starred revie..."
+      ]
+    },
+    guid: {
+      rendered: "https://www.kirkusreviews.com/book-reviews/sarah-braunstein/baby-in-a-box/"
+    }
+  },
+  {
+    yoast_head_json: {
+      og_image: [
+        {
+          url: "",
+          src: ""
+        }
+      ],
+      og_description: ""
+    },
+    title: {
+      rendered: "Plesch&#8217;s Contributes to the Spring 2026 &lt;em>Maine Arts Journal&lt;/em>"
+    },
+    ['post-meta-fields']: {
+      primary_category: "",
+      summary: [
+        "James M. Gillespie Professor of Art Véronique Plesch edited the spring issue of <em>Maine Arts Journal</em>. As usual, s..."
+      ]
+    },
+    guid: {
+      rendered: "https://maineartsjournal.com/veronique-plesch-introduction-spring-2026-the-shape-of-time/"
+    }
+  }
 ];
+
+
 
 // The default export metadata for your component
 export default {
@@ -91,13 +131,13 @@ export default {
 export const Primary = {
   name: "Article Section",
   args: {
-    renderApi: true,
-    api: "Latest News",
-    perView: 1,
-  },
-  play: async ({ canvasElement }) => {
-    await expect(
-      canvasElement.querySelector(".article-section"),
-    ).not.toBeNull();
+    render_api: false,
+    perView: 2,
+    initial_items: mockItems,
+    heading: "News",
+    paragraph: "Get updates from the department",
+    hydrated_from_server: false,
+    display_posts_method: 'internal',
+    carousel: true
   },
 };
