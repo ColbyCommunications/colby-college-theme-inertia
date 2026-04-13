eda7cfc12dc9433217109c788850767348dfd8af
Brandon K. Waltz <bwaltz6@gmail.com>
2026-04-13 14:43:35 -0400

    Update BgInsetMediaContextBlock.stories.js

--- resources/js/components/BgInsetMediaContext/storybook/BgInsetMediaContextBlock.stories.js
+++ resources/js/components/BgInsetMediaContext/storybook/BgInsetMediaContextBlock.stories.js
@@ -11,14 +11,7 @@ const globalArgs = {
   heading: "Lorem Ipsum",
   subheading: "Veritatis sit voluptatem",
   image: {
-    src: "https://placeholdit.com/600x400/dddddd/999999",
-    srcset:
-      "https://placeholdit.com/300x200/dddddd/999999 300w, https://placeholdit.com/600x400/dddddd/999999 600w",
-    alt: "this is an alt tag",
-    sizes: {
-      Hero: "https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg",
-      mobile: "https://placehold.co/300x200",
-    },
+    url: "https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg",
   },
   paragraph:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
