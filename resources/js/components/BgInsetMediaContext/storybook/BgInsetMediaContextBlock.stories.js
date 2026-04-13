61ff6e8a13416532af7fd443544b29bfe1998998
Brandon K. Waltz <bwaltz6@gmail.com>
2026-04-13 16:05:08 -0400

    Update BgInsetMediaContextBlock.stories.js

--- resources/js/components/BgInsetMediaContext/storybook/BgInsetMediaContextBlock.stories.js
+++ resources/js/components/BgInsetMediaContext/storybook/BgInsetMediaContextBlock.stories.js
@@ -7,35 +7,18 @@ export default {
   component: BgInsetMediaContext,
 };
 
-const globalArgs = {
-  heading: "Lorem Ipsum",
-  subheading: "Veritatis sit voluptatem",
-  image: {
-    url: "https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg",
-  },
-  paragraph:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-};
-
-const assertHeadingVisible = async ({ canvas, canvasElement }) => {
-  const first = canvasElement.querySelectorAll(".media-context__context")[0];
-  const firstHeading = first.querySelectorAll(".text-group__heading")[0];
-  const firstSubHeading = first.querySelectorAll(".text-group__subheading")[0];
-  await expect(firstHeading.textContent.slice(0, -1)).toBe("Lorem Ipsum");
-  await expect(firstHeading).toBeVisible();
-
-  await expect(firstSubHeading.textContent.slice(0, -1)).toBe(
-    "Veritatis sit voluptatem",
-  );
-  await expect(firstSubHeading).toBeVisible();
-};
 
 export const Primary = {
   // 👇 Rename this story
   name: "Default",
   tags: ["!autodocs"],
   args: {
-    ...globalArgs,
+    heading: "Lorem Ipsum",
+    subheading: "Veritatis sit voluptatem",
+    image: {
+      url: "https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg",
+    },
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
-  play: assertHeadingVisible,
 };
