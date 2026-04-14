e921bf0d494265bb7a9141d8cd66c132ebed7205
Brandon K. Waltz <bwaltz6@gmail.com>
2026-04-14 12:33:52 -0400

    Update app.php

--- app.php
+++ app.php
@@ -3,6 +3,10 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="preconnect" href="https://use.typekit.net" crossorigin>
+  <link rel="preconnect" href="https://p.typekit.net" crossorigin>
+  <link rel="preload" href="https://use.typekit.net/iyv0bye.css" as="style">
+  <link rel="stylesheet" href="https://use.typekit.net/iyv0bye.css">  
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
