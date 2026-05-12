82c96c6974a8db0d787be927f1facf08120f763e
bwaltz <bwaltz6@gmail.com>
2026-05-12 20:04:21 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@9849215bbafa8c491c4b5d187136ee70e5cf0162 🚀

---
+++ assets/formatter-EIJCOSYU-DAFI_zAh.js
@@ -0,0 +1,1 @@
+import{d as m,c as e}from"./blocks-xbw5V_12.js";import{x as o}from"./iframe-CA7QXw-f.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CE1G-McA.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
