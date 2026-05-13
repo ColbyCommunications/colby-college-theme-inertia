07a897bceaedba0e27cdb3a9186d8c1ead12b013
GioAceto <55949483+GioAceto@users.noreply.github.com>
2026-05-13 14:22:39 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@a5baa63a47282685915dc81f8d059c7769ec1e84 🚀

---
+++ assets/BgInsetMediaContextBlock.stories-_bdXHSL5.js
@@ -0,0 +1,17 @@
+import{_ as t}from"./BgInsetMediaContext-DIr1pyES.js";import"./iframe-C_vbhBJc.js";import"./preload-helper-PPVm8Dsz.js";import"./MediaContext-DB7KUXsg.js";import"./Video-DB2XHBV4.js";import"./_commonjsHelpers-CE1G-McA.js";import"./Icon-BwYRw0zD.js";import"./Picture-aVz7hnkD.js";import"./Context-CrB_8Uwu.js";import"./TextGroup-ap6FHcKh.js";import"./noframework.waypoints-DVP5Ss7z.js";import"./ButtonGroup-CTO0P2W4.js";import"./Button-BVTvZbw5.js";const{expect:b}=__STORYBOOK_MODULE_TEST__,h={title:"Blocks/Background Inset Media Context",component:t,argTypes:{type:{name:"Type",control:"inline-radio",options:["Dark","Light"],table:{category:"Wordpress Fields"}},inset:{name:"Inset",control:"check",options:["Inset"],table:{category:"Wordpress Fields"}},size:{name:"Size",control:"inline-radio",options:["Medium","Large"],table:{category:"Wordpress Fields"}},reverse:{name:"Reverse",control:"check",options:["Reverse"],table:{category:"Wordpress Fields"}},heading:{name:"Heading",control:"text",table:{category:"Wordpress Fields"}},subheading:{name:"Subheading",control:"text",table:{category:"Wordpress Fields"}},paragraph:{name:"Paragraph",control:"text",table:{category:"Wordpress Fields"}},image:{name:"Image",control:"object",table:{category:"Wordpress Fields"}},video:{name:"Video",control:"object",table:{category:"Wordpress Fields"}}}},e={name:"Background Inset Media Context",tags:["!autodocs"],args:{type:"Dark",inset:"",size:"Medium",reverse:"",image:{url:"https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg"},video:{},subheading:"Veritatis sit voluptatem",heading:"Lorem Ipsum",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: "Background Inset Media Context",
+  tags: ["!autodocs"],
+  args: {
+    type: "Dark",
+    inset: "",
+    size: "Medium",
+    reverse: "",
+    image: {
+      url: "https://www.colby.edu/wp-content/uploads/2025/12/Untitled-2025-12-18T101409.127-2400x1320.jpeg"
+    },
+    video: {},
+    subheading: "Veritatis sit voluptatem",
+    heading: "Lorem Ipsum",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+}`,...e.parameters?.docs?.source}}};const x=["Primary"];export{e as Primary,x as __namedExportsOrder,h as default};
