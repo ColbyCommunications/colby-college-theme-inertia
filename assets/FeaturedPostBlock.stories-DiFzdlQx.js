73d68e84bba00d29a84050b145c1139fc7d250a9
bwaltz <bwaltz6@gmail.com>
2026-05-22 14:58:20 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@13df29388acd2810cbc69ffc6b1196fc5044604b 🚀

---
+++ assets/FeaturedPostBlock.stories-DiFzdlQx.js
@@ -0,0 +1,29 @@
+import e from"./FeaturedPost-Cvi90Q2m.js";import"./iframe-B7FQuczq.js";import"./preload-helper-PPVm8Dsz.js";import"./Context-BcJPGzyp.js";import"./TextGroup-9hPkyUVP.js";import"./noframework.waypoints-DVP5Ss7z.js";import"./ButtonGroup-C2HfrL6i.js";import"./Button-ByfsewhL.js";import"./AnimatedBorder-DZfPh0QC.js";import"./index-D4-8ALNT.js";import"./Picture-BZWUJCP_.js";const{expect:u}=__STORYBOOK_MODULE_TEST__,g={title:"Blocks/Featured Post",component:e,tags:["!autodocs"],argTypes:{heading:{name:"Heading",control:"text",table:{category:"Wordpress Fields"}},paragraph:{name:"Paragraph",control:"text",table:{category:"Wordpress Fields"}},caption:{name:"Caption",control:"text",table:{category:"Wordpress Fields"}},buttons:{name:"Buttons",control:"object",table:{category:"Wordpress Fields"}},post:{name:"Post",control:"object",table:{category:"Wordpress Fields"}}}},t={name:"Featured Post",args:{heading:"Events",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",caption:"Allen & Benner",buttons:[{url:"#",title:"All Events"}],post:{date:"June 24, 2022",heading:"Lorem ipsum dolor sit amet, consectet.",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",buttons:[{url:"#",title:"All Events"}],image:{srcset:"https://placehold.co/760x430",src:"https://placehold.co/410x290",alt:"Featured post image",sizes:{Rectangle:"https://placehold.co/760x430"}}}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  name: "Featured Post",
+  args: {
+    heading: "Events",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",
+    caption: "Allen & Benner",
+    buttons: [{
+      url: "#",
+      title: "All Events"
+    }],
+    post: {
+      date: "June 24, 2022",
+      heading: "Lorem ipsum dolor sit amet, consectet.",
+      paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",
+      buttons: [{
+        url: "#",
+        title: "All Events"
+      }],
+      image: {
+        srcset: "https://placehold.co/760x430",
+        src: "https://placehold.co/410x290",
+        alt: "Featured post image",
+        sizes: {
+          Rectangle: "https://placehold.co/760x430"
+        }
+      }
+    }
+  }
+}`,...t.parameters?.docs?.source}}};const h=["Default"];export{t as Default,h as __namedExportsOrder,g as default};
---
+++ assets/OverlayHeroBlock.stories-DZGQI7cI.js
@@ -0,0 +1,35 @@
+import a from"./OverlayHero-BQhcoLAf.js";import"./iframe-B7FQuczq.js";import"./preload-helper-PPVm8Dsz.js";import"./Context-BcJPGzyp.js";import"./TextGroup-9hPkyUVP.js";import"./noframework.waypoints-DVP5Ss7z.js";import"./ButtonGroup-C2HfrL6i.js";import"./Button-ByfsewhL.js";import"./Picture-BZWUJCP_.js";import"./Video-DZclTnMb.js";import"./_commonjsHelpers-DsqdWQfm.js";import"./Icon-DSzGxKLe.js";import"./HlsBackground-BfAOQNdA.js";const{expect:o}=__STORYBOOK_MODULE_TEST__,y={title:"Blocks/Overlay Hero",component:a,tags:["!autodocs"],argTypes:{subheading:{name:"Subheading",control:"text",table:{category:"Wordpress Fields"}},heading:{name:"Heading",control:"text",table:{category:"Wordpress Fields"}},paragraph:{name:"Paragraph",control:"text",table:{category:"Wordpress Fields"}},buttons:{name:"Buttons",control:"object",table:{category:"Wordpress Fields"}},videoLoop:{name:"Video Loop",control:"object",table:{category:"Wordpress Fields"}},image:{name:"Image",control:"object",table:{category:"Wordpress Fields"}},video:{name:"Video",control:"object",table:{category:"Wordpress Fields"}},fromPage:{table:{disable:!0}},size:{table:{disable:!0}}}},e={name:"Overlay Hero",args:{subheading:"Welcome to",heading:"Colby College",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",buttons:[{button:{url:"#",title:"Learn More",target:""}}],videoLoop:{video:"ExampleVideo.mp4"},image:{src:"https://placehold.co/1280x720",srcset:"https://placehold.co/1280x720",alt:"Campus view",sizes:{Hero:"https://placehold.co/1280x720"}},video:null,fromPage:!0},play:async({canvas:t})=>{await o(t.getByText("Welcome to")).toBeInTheDocument(),await o(t.getByText("Colby College")).toBeInTheDocument(),await o(t.getByAltText("Campus view")).toBeInTheDocument()}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: "Overlay Hero",
+  args: {
+    subheading: "Welcome to",
+    heading: "Colby College",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    buttons: [{
+      button: {
+        url: "#",
+        title: "Learn More",
+        target: ""
+      }
+    }],
+    videoLoop: {
+      video: "ExampleVideo.mp4"
+    },
+    image: {
+      src: "https://placehold.co/1280x720",
+      srcset: "https://placehold.co/1280x720",
+      alt: "Campus view",
+      sizes: {
+        Hero: "https://placehold.co/1280x720"
+      }
+    },
+    video: null,
+    fromPage: true
+  },
+  play: async ({
+    canvas
+  }) => {
+    await expect(canvas.getByText("Welcome to")).toBeInTheDocument();
+    await expect(canvas.getByText("Colby College")).toBeInTheDocument();
+    await expect(canvas.getByAltText("Campus view")).toBeInTheDocument();
+  }
+}`,...e.parameters?.docs?.source}}};const x=["WithImage"];export{e as WithImage,x as __namedExportsOrder,y as default};
