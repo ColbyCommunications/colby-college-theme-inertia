fe4fa78481f4876fc4a66795143c9ece4550de32
Brandon K. Waltz <bwaltz6@gmail.com>
2026-05-26 14:04:14 -0400

    Update ImageText.vue

--- resources/js/components/ImageText/ImageText.vue
+++ resources/js/components/ImageText/ImageText.vue
@@ -29,7 +29,7 @@
         </figcaption>
       </div>
 
-      <div v-html="paragraph_text" class="font-body font-normal text-16 leading-[1.75] text-left text-indigo-800 mt-2 [&>p]:mb-2 [&_p>a:only-child]:min-h-[44px] [&_p>a:only-child]:inline-block [&_a]:underline [&_a:hover]:no-underline [&_a:hover]:text-indigo" ></div>
+      <div v-html="paragraph_text" class="font-body font-normal text-16 leading-[1.75] text-left text-indigo-800 mt-2 [&>p]:mb-2 [&_p>a:only-child]:min-h-[44px] [&_p>a:only-child]:inline-block [&_a]:underline [&_a:hover]:no-underline [&_a:hover]:text-indigo [&>p]:mb-4" ></div>
     </div>
 
     <!-- Side-by-side mode -->
