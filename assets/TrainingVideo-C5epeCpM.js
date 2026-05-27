a1291852852e1ac713efa94cc3f9c4a511552f38
bwaltz <bwaltz6@gmail.com>
2026-05-27 20:02:54 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@813107bec4d7ce3fa307d51715d97e17df22767d 🚀

--- assets/TrainingVideo-C5epeCpM.js
+++
@@ -1,2 +0,0 @@
-import{u as r,k as t,f as i}from"./blocks-D1iSaySD.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-DsqdWQfm.js";import"./iframe-Dexjh89f.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Blocks/Bordered Article Row/Training Video"}),`
-`,t.jsx("video",{controls:!0,children:t.jsx("source",{src:"https://www.colby.edu/communications/training-videos/Blocks_Accordion_final.mp4",type:"video/mp4"})})]})}function m(o={}){const{wrapper:e}={...r(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(n,{...o})}):n()}export{m as default};
