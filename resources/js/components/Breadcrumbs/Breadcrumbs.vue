e4dbe49eaaf02c58c19a33b16a53b23e936c0602
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2026-06-09 10:33:25 -0400

    fix html entities in breadcrumbs issue

--- resources/js/components/Breadcrumbs/Breadcrumbs.vue
+++ resources/js/components/Breadcrumbs/Breadcrumbs.vue
@@ -1,23 +1,20 @@
 <template>
   <nav
     aria-label="Breadcrumbs"
-    class="breadcrumbs flex h-9 items-center overflow-x-auto bg-gray-100 min-h-[44px] min-w-[44px]"
+    class="breadcrumbs flex h-9 min-h-[44px] min-w-[44px] items-center overflow-x-auto bg-gray-100"
   >
     <div class="breadcrumbs__inner mx-auto w-full max-w-screen-2xl px-5">
       <ul class="flex items-center [&>li+li]:ml-5">
         <li
           class="font-body text-10 leading-140 font-bold whitespace-nowrap text-indigo-800"
         >
-        <a
-          class="inline-flex h-[44px] w-[44px] items-center justify-center text-indigo-800 transition-all duration-200 ease-in-out hover:text-indigo"
-          href="/"
-        >
+          <a
+            class="inline-flex h-[44px] w-[44px] items-center justify-center text-indigo-800 transition-all duration-200 ease-in-out hover:text-indigo"
+            href="/"
+          >
             <span class="sr-only">Homepage</span>
 
-            <Icon
-              :name="'home'"
-              class="size-[10px] fill-indigo"
-            />
+            <Icon :name="'home'" class="size-[10px] fill-indigo" />
           </a>
         </li>
 
@@ -27,11 +24,10 @@
           class="font-body text-10 leading-140 font-bold whitespace-nowrap text-indigo-800"
         >
           <a
-            class="text-indigo-800 transition-all duration-200 ease-in-out hover:text-indigo min-h-[44px] min-w-[44px] leading-[44px] inline-block"
+            class="inline-block min-h-[44px] min-w-[44px] leading-[44px] text-indigo-800 transition-all duration-200 ease-in-out hover:text-indigo"
             :href="item.url"
-          >
-            {{ item.title }}
-          </a>
+            v-html="item.title"
+          ></a>
         </li>
       </ul>
     </div>
