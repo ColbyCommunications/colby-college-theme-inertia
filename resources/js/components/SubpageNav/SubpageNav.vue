2342abdc75879ade4195bbbd6f70a09dce7265c8
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2026-06-09 14:21:39 -0400

    add widget list spacing and fix sidebar text wrap spacing

--- resources/js/components/SubpageNav/SubpageNav.vue
+++ resources/js/components/SubpageNav/SubpageNav.vue
@@ -1,31 +1,31 @@
 <template>
   <div
-    class="subpage-nav md:space-y-5 bg-white md:bg-transparent border md:border-0 border-solid border-indigo-300 rounded-md"
+    class="subpage-nav rounded-md border border-solid border-indigo-300 bg-white md:space-y-5 md:border-0 md:bg-transparent"
     :class="{ 'is-bot-visitor': isBot }"
     @click.self="toggleMenu"
   >
     <div ref="container" class="subpage-nav--animated">
       <h2>
         <div
-          class="relative w-full px-6 md:px-0 py-4 md:py-0 font-extended font-bold text-14 md:text-12 leading-130 text-azure tracking-8 uppercase text-left"
+          class="relative w-full px-6 py-4 text-left font-extended text-14 leading-130 font-bold tracking-8 text-azure uppercase md:px-0 md:py-0 md:text-12"
           @click="toggleMenu"
         >
           <a
             :href="parentPermalink"
-            class="pointer-events-none md:pointer-events-auto min-h-[44px] min-w-[44px] leading-[44px] inline-block"
+            class="pointer-events-none inline-block min-h-[44px] min-w-[44px] leading-[44px] md:pointer-events-auto"
           >
             {{ heading }}
-            <div class="absolute top-6 right-8 md:hidden w-1.5">
+            <div class="absolute top-6 right-8 w-1.5 md:hidden">
               <span
-                class="relative block w-6 h-0.5 bg-azure transition-all duration-200 ease-in-out"
+                class="relative block h-0.5 w-6 bg-azure transition-all duration-200 ease-in-out"
                 :class="{ 'bg-transparent': menuOpen }"
               >
                 <span
-                  class="absolute left-0 w-full h-full bg-azure origin-center top-[-6px] transition-all duration-200 ease-in-out"
+                  class="absolute top-[-6px] left-0 h-full w-full origin-center bg-azure transition-all duration-200 ease-in-out"
                   :class="{ '!top-0 rotate-45': menuOpen }"
                 ></span>
                 <span
-                  class="absolute left-0 w-full h-full bg-azure origin-center top-[6px] transition-all duration-200 ease-in-out"
+                  class="absolute top-[6px] left-0 h-full w-full origin-center bg-azure transition-all duration-200 ease-in-out"
                   :class="{ '!top-0 rotate-[-45deg]': menuOpen }"
                 ></span>
               </span>
@@ -35,19 +35,19 @@
       </h2>
       <ul
         v-if="items.length > 0"
-        class="subpage-nav__items md:block py-4 md:py-0 border-t md:border-t-0 border-solid border-[#eef4ff] mt-0 md:mt-6"
+        class="subpage-nav__items mt-0 border-t border-solid border-[#eef4ff] py-4 md:mt-6 md:block md:border-t-0 md:py-0"
         :class="{ hidden: !menuOpen && !isBot }"
       >
         <li
           v-for="(item, index) in items"
           :key="index"
-          class="subpage-nav__item font-body text-14 md:text-10 font-normal md:font-medium leading-140 text-indigo-800"
+          class="subpage-nav__item font-body text-14 leading-140 font-normal text-indigo-800 md:text-10 md:font-medium"
         >
           <a
-            class="block min-h-[44px] min-w-[44px] leading-[44px] px-6 md:px-0 md:pl-2.5 hover:bg-[#eef4ff] md:border-l-2 border-solid transition-all duration-200 ease-in-out"
+            class="block flex min-h-[44px] min-w-[44px] items-center border-solid px-6 transition-all duration-200 ease-in-out hover:bg-[#eef4ff] md:border-l-2 md:px-0 md:pl-2.5"
             :class="{
-              'text-gray-1000 border-indigo': item.active,
-              'text-indigo-800 hover:text-indigo hover:underline border-transparent':
+              'border-indigo text-gray-1000': item.active,
+              'border-transparent text-indigo-800 hover:text-indigo hover:underline':
                 !item.active,
             }"
             :href="item.url"
--- resources/js/components/Widget/Widget.vue
+++ resources/js/components/Widget/Widget.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="widget p-5 bg-gray-100 space-y-3">
+  <div class="widget space-y-3 bg-gray-100 p-5">
     <h2
       v-if="heading"
-      class="font-extended font-bold text-14 md:text-12 leading-130 text-azure tracking-8 uppercase"
+      class="font-extended text-14 leading-130 font-bold tracking-8 text-azure uppercase md:text-12"
     >
       {{ heading }}
     </h2>
     <div
-      class="widget__context [&>p]:font-body [&>p]:font-medium [&>p]:text-10 [&>p]:leading-[1.5] [&>p>a]:no-underline hover:[&>p>a]:underline [&>p]:text-indigo-800 hover:[&>p>a]:text-indigo [&>ul>li]:font-body [&>ul>li]:font-medium [&>ul>li]:text-10 [&>ul>li]:leading-140 [&>ul>li>a]:no-underline [&>ul>li>a:hover]:underline [&>ul>li]:text-indigo-800 [&>ul>li>a:hover]:text-indigo [&>ul>li]:list-disc [&>ul>li]:ml-3 [&>ul>li+li]:mt-2 space-y-2"
+      class="list-spacing widget__context space-y-2 [&>p]:font-body [&>p]:text-10 [&>p]:leading-[1.5] [&>p]:font-medium [&>p]:text-indigo-800 [&>p>a]:no-underline hover:[&>p>a]:text-indigo hover:[&>p>a]:underline [&>ul>li]:ml-3 [&>ul>li]:list-disc [&>ul>li]:font-body [&>ul>li]:text-10 [&>ul>li]:leading-140 [&>ul>li]:font-medium [&>ul>li]:text-indigo-800 [&>ul>li>a]:no-underline [&>ul>li>a:hover]:text-indigo [&>ul>li>a:hover]:underline"
       v-html="text"
     ></div>
     <ButtonGroup
