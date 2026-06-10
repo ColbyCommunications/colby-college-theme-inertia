d52f15a5687ad06e37ab4d0d9af2971e8c8cbbf3
GioAceto <55949483+GioAceto@users.noreply.github.com>
2026-06-10 19:11:11 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@c23cbed1069a073d0be5ee9aeb0da4c8b3033d5d 🚀

---
+++ assets/OverlayHeroBlock.stories-CTxIJdN0.js
@@ -0,0 +1,35 @@
+import a from"./OverlayHero-BtFjkSt2.js";import"./iframe-CvDUikKS.js";import"./preload-helper-PPVm8Dsz.js";import"./Context-Ddw2iSGu.js";import"./TextGroup-CaW9w8Am.js";import"./noframework.waypoints-DVP5Ss7z.js";import"./ButtonGroup-DPUeTbAX.js";import"./Button-z-AqZp8O.js";import"./Picture-Con6zQ8w.js";import"./Video-CYdtJp7F.js";import"./_commonjsHelpers-CE1G-McA.js";import"./Icon-wsDgXQT8.js";import"./HlsBackground-E4H-u7xb.js";const{expect:o}=__STORYBOOK_MODULE_TEST__,y={title:"Blocks/Overlay Hero",component:a,tags:["!autodocs"],argTypes:{subheading:{name:"Subheading",control:"text",table:{category:"Wordpress Fields"}},heading:{name:"Heading",control:"text",table:{category:"Wordpress Fields"}},paragraph:{name:"Paragraph",control:"text",table:{category:"Wordpress Fields"}},buttons:{name:"Buttons",control:"object",table:{category:"Wordpress Fields"}},videoLoop:{name:"Video Loop",control:"object",table:{category:"Wordpress Fields"}},image:{name:"Image",control:"object",table:{category:"Wordpress Fields"}},video:{name:"Video",control:"object",table:{category:"Wordpress Fields"}},fromPage:{table:{disable:!0}},size:{table:{disable:!0}}}},e={name:"Overlay Hero",args:{subheading:"Welcome to",heading:"Colby College",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",buttons:[{button:{url:"#",title:"Learn More",target:""}}],videoLoop:{video:"ExampleVideo.mp4"},image:{src:"https://placehold.co/1280x720",srcset:"https://placehold.co/1280x720",alt:"Campus view",sizes:{Hero:"https://placehold.co/1280x720"}},video:null,fromPage:!0},play:async({canvas:t})=>{await o(t.getByText("Welcome to")).toBeInTheDocument(),await o(t.getByText("Colby College")).toBeInTheDocument(),await o(t.getByAltText("Campus view")).toBeInTheDocument()}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: "Overlay Hero",
+  args: {
+    subheading: "Welcome to",
+    heading: "Colby College",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    buttons: [{
+      button: {
+        url: "#",
+        title: "Learn More",
+        target: ""
+      }
+    }],
+    videoLoop: {
+      video: "ExampleVideo.mp4"
+    },
+    image: {
+      src: "https://placehold.co/1280x720",
+      srcset: "https://placehold.co/1280x720",
+      alt: "Campus view",
+      sizes: {
+        Hero: "https://placehold.co/1280x720"
+      }
+    },
+    video: null,
+    fromPage: true
+  },
+  play: async ({
+    canvas
+  }) => {
+    await expect(canvas.getByText("Welcome to")).toBeInTheDocument();
+    await expect(canvas.getByText("Colby College")).toBeInTheDocument();
+    await expect(canvas.getByAltText("Campus view")).toBeInTheDocument();
+  }
+}`,...e.parameters?.docs?.source}}};const x=["WithImage"];export{e as WithImage,x as __namedExportsOrder,y as default};
---
+++ assets/ParagraphBlock.stories-CKt_Ks_8.js
@@ -0,0 +1,33 @@
+import o from"./Paragraph-DOTyQLjX.js";import"./iframe-CvDUikKS.js";import"./preload-helper-PPVm8Dsz.js";import"./ButtonGroup-DPUeTbAX.js";import"./Button-z-AqZp8O.js";import"./TextGroup-CaW9w8Am.js";import"./noframework.waypoints-DVP5Ss7z.js";const{expect:n,waitFor:s}=__STORYBOOK_MODULE_TEST__,x={title:"Blocks/Paragraph",component:o,tags:["!autodocs"],argTypes:{paragraph_text:{name:"Text",control:"text",table:{category:"Wordpress Fields"}},buttons:{name:"Buttons",table:{category:"Wordpress Fields"}}}},r="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed leo odio, ultrices quis auctor eu, fermentum at dolor. Vivamus cursus lorem ut ultrices iaculis.",a={name:"Paragraph",args:{paragraph_text:r},play:async({canvas:t})=>{await n(t.getByText(r)).toBeInTheDocument()}},e={name:"With Buttons",args:{paragraph_text:r,buttons:[{button:{url:"#",title:"Learn More",target:""}}]},play:async({canvas:t})=>{await n(t.getByText(r)).toBeInTheDocument(),await s(async()=>{await n(t.getByText("Learn More")).toBeVisible()},{timeout:3e3})}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  name: "Paragraph",
+  args: {
+    paragraph_text: paragraphText
+  },
+  play: async ({
+    canvas
+  }) => {
+    await expect(canvas.getByText(paragraphText)).toBeInTheDocument();
+  }
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: "With Buttons",
+  args: {
+    paragraph_text: paragraphText,
+    buttons: [{
+      button: {
+        url: "#",
+        title: "Learn More",
+        target: ""
+      }
+    }]
+  },
+  play: async ({
+    canvas
+  }) => {
+    await expect(canvas.getByText(paragraphText)).toBeInTheDocument();
+    await waitFor(async () => {
+      await expect(canvas.getByText("Learn More")).toBeVisible();
+    }, {
+      timeout: 3000
+    });
+  }
+}`,...e.parameters?.docs?.source}}};const h=["Default","WithButtons"];export{a as Default,e as WithButtons,h as __namedExportsOrder,x as default};
