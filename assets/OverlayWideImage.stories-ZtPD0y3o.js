bc7995af4e5db025d8914140050d1749ae709d5e
bwaltz <bwaltz6@gmail.com>
2026-06-16 20:52:41 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@7b9a45dbba837d4334594cfd5c4bf629e1c91e38 🚀

---
+++ assets/OverlayWideImage.stories-ZtPD0y3o.js
@@ -0,0 +1,28 @@
+import t from"./OverlayWideImage-6FYdFrC0.js";import"./iframe-DAsnk83b.js";import"./preload-helper-PPVm8Dsz.js";import"./Context-CndsJk52.js";import"./TextGroup-Cjq_dFuI.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./noframework.waypoints-DVP5Ss7z.js";import"./ButtonGroup-BukZTuxa.js";import"./Button-3yFMyosy.js";import"./Picture-CJAzcxhc.js";const{expect:u}=__STORYBOOK_MODULE_TEST__,h={title:"Core Components/Overlay Wide Image",component:t,tags:["autodocs"]},e={name:"Default",args:{subheading:"Green Colby",heading:"Sustainability and Stewardship",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",buttons:[{url:"#",title:"Go Green at Colby"}],image:{srcset:"https://placehold.co/880x400",src:"https://placehold.co/400x400",alt:"Placeholder image",sizes:{Landscape:"https://placehold.co/880x400"}}},play:async({canvas:a})=>{}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: "Default",
+  args: {
+    subheading: "Green Colby",
+    heading: "Sustainability and Stewardship",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",
+    buttons: [{
+      url: "#",
+      title: "Go Green at Colby"
+    }],
+    image: {
+      srcset: "https://placehold.co/880x400",
+      src: "https://placehold.co/400x400",
+      alt: "Placeholder image",
+      sizes: {
+        Landscape: "https://placehold.co/880x400"
+      }
+    }
+  },
+  play: async ({
+    canvas
+  }) => {
+    if (import.meta.env.MODE !== 'test') return;
+    await expect(canvas.getByText("Green Colby")).toBeInTheDocument();
+    await expect(canvas.getByText("Sustainability and Stewardship")).toBeInTheDocument();
+    await expect(canvas.getByText("Go Green at Colby")).toBeInTheDocument();
+  }
+}`,...e.parameters?.docs?.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,h as default};
