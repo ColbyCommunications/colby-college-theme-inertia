f64595808c63492d6e8fd1c73d4978c2015e7852
Brandon K. Waltz <bwaltz6@gmail.com>
2026-06-26 20:23:57 -0400

    Update TableSection.vue

--- resources/js/components/TableSection/TableSection.vue
+++ resources/js/components/TableSection/TableSection.vue
@@ -2,7 +2,7 @@
   <div class="table-section py-20 bg-cloud">
     <div class="table-section__inner md:grid md:grid-cols-12 gap-x-10 max-w-screen-2xl w-full px-5 my-0 mx-auto">
       <div class="md:col-start-3 md:col-span-8">
-        <Table v-bind="table" />
+        <Table v-bind="table" :manualItems="table.items" />
       </div>
     </div>
   </div>
@@ -11,7 +11,8 @@
 <script setup>
 import Table from "@/js/components/Table/Table.vue";
 
-defineProps({
+const props = defineProps({
   table: { type: Object, default: () => ({}) },
 });
+
 </script>
