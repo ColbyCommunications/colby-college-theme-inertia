8f8ec9af08b22b06bcaccd90941c30860e74e8fe
bwaltz <bwaltz6@gmail.com>
2026-08-25 19:28:21 +0000

    Deploying to gh-pages from @ ColbyCommunications/colby-college-theme-inertia@060cf8fa8e9bd7f5b010121c4efd53c9c445b3f8 🚀

---
+++ master/assets/FeaturedPostBlock.stories-rFwoKFqF.js
@@ -0,0 +1,29 @@
+import e from"./FeaturedPost-DWgSv0ge.js";import"./iframe-CX-bhEdk.js";import"./preload-helper-CNn__nYE.js";import"./Context-B-t8ywx5.js";import"./TextGroup-BBJYvtxw.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./noframework.waypoints-DVP5Ss7z.js";import"./index-xgxdCp6f.js";import"./ButtonGroup-DC9boJ30.js";import"./Button-DZFNNaTr.js";import"./AnimatedBorder-5qtPCxZa.js";import"./Picture-BHyYZoRE.js";const{expect:g}=__STORYBOOK_MODULE_TEST__,h={title:"Blocks/Featured Post",component:e,tags:["!autodocs"],argTypes:{heading:{name:"Heading",control:"text",table:{category:"Wordpress Fields"}},paragraph:{name:"Paragraph",control:"text",table:{category:"Wordpress Fields"}},caption:{name:"Caption",control:"text",table:{category:"Wordpress Fields"}},buttons:{name:"Buttons",control:"object",table:{category:"Wordpress Fields"}},post:{name:"Post",control:"object",table:{category:"Wordpress Fields"}}}},t={name:"Featured Post",args:{heading:"Events",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",caption:"Allen & Benner",buttons:[{url:"#",title:"All Events"}],post:{date:"June 24, 2022",heading:"Lorem ipsum dolor sit amet, consectet.",paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",buttons:[{url:"#",title:"All Events"}],image:{srcset:"https://placehold.co/760x430",src:"https://placehold.co/410x290",alt:"Featured post image",sizes:{Rectangle:"https://placehold.co/760x430"}}}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  name: "Featured Post",
+  args: {
+    heading: "Events",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",
+    caption: "Allen & Benner",
+    buttons: [{
+      url: "#",
+      title: "All Events"
+    }],
+    post: {
+      date: "June 24, 2022",
+      heading: "Lorem ipsum dolor sit amet, consectet.",
+      paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.",
+      buttons: [{
+        url: "#",
+        title: "All Events"
+      }],
+      image: {
+        srcset: "https://placehold.co/760x430",
+        src: "https://placehold.co/410x290",
+        alt: "Featured post image",
+        sizes: {
+          Rectangle: "https://placehold.co/760x430"
+        }
+      }
+    }
+  }
+}`,...t.parameters?.docs?.source}}};const b=["Default"];export{t as Default,b as __namedExportsOrder,h as default};
